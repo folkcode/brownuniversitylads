b9dfda14ebee983e69e4c3d188bf9cce22e5b31b
ladsgaribaldi <ladsgaribaldi@gmail.com>
2012-04-19 17:32:43 +0000

    mashby - added Michael's in app CSV functionality to harvard version

--- SurfaceApplication3/MainWindow.xaml.cs
+++ SurfaceApplication3/MainWindow.xaml.cs
@@ -180,5 +180,12 @@ namespace SurfaceApplication3
             Application.Current.Shutdown();
         }
 
+        private void CSVImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            CSVImportDialog csv_dialog = new CSVImportDialog();
+            csv_dialog.WindowState = System.Windows.WindowState.Normal;
+            csv_dialog.ShowDialog();
+        }
+
     }
 }
